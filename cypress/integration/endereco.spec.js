cf858dc4e4296271a3fa1bde379efb6a3e8b53f8
Carina Souza <carinasfs@hotmail.com>
2022-03-09 18:13:51 +0000

    Novas funcionalidades

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 ///<reference types="cypress"/>
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require ('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -8,12 +10,30 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
         })
 
-        
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Marcia', 'Souza', 'Google', 'Brasil', 'Av Brasil', '566', 'Rio de Janeiro', 'Rio de Janeiro', '21321200', '21978546263', 'teste@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
-        //cadastro de endereço
-        
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
+    });
+
 });
